de0c30120d9881df4178ee2d01ba6d6a8a2bfe99
edgarGD <edgar.ai@senome.games>
2024-07-08 08:17:59 +0300

    README for PONG updated, also add option to control paddles with uppercase letters

--- projects/P01_Pong/README.md
+++ projects/P01_Pong/README.md
@@ -1,3 +1,10 @@
 # Pong was the first project I implemented on C in school21
 
 ![screenshot](/projects/P01_Pong/pic/pong.png)
+
+To build and launch Pong you need to download the repository and build the project with command:
+```make```
+
+After that the game will start.
+Press the z and x buttons to control the left paddle, and the m and n buttons to control the right paddle.
+The game will be finished if one of the two players scores 10 points.
--- projects/P01_Pong/src/pad.c
+++ projects/P01_Pong/src/pad.c
@@ -2,12 +2,12 @@
 
 void pad_move(short int *pad1y, short int *pad2y) {
   char c = getch();
-  if (c == 'z' && *pad1y != HEIGHT - 3)
+  if ((c == 'z' || c == 'Z') && *pad1y != HEIGHT - 3)
     (*pad1y)++;
-  else if (c == 'x' && *pad1y != 3)
+  else if ((c == 'x' || c == 'X') && *pad1y != 3)
     (*pad1y)--;
-  else if (c == 'n' && *pad2y != HEIGHT - 3)
+  else if ((c == 'n' || c == 'N') && *pad2y != HEIGHT - 3)
     (*pad2y)++;
-  else if (c == 'm' && *pad2y != 3)
+  else if ((c == 'm' || c == 'M') && *pad2y != 3)
     (*pad2y)--;
 }
